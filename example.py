b9c48b883591811f0de3bfa282c3b3cfeb64312e
lukasz-segin <lukasz-segin@o2.pl>
2021-11-26 13:26:02 +0100

    Create json with data, create simple pydantic model Book and print data from json file

--- example.py
+++ example.py
@@ -1,6 +1,30 @@
-def say_hello():
-    print("hello")
+"""
+Basic example showing how to read and validate data from a file using Pydantic.
+"""
+
+import json
+import pydantic
+from typing import Optional
+
+
+class Book(pydantic.BaseModel):
+
+    title: str
+    author: str
+    publisher: str
+    price: float
+    isbn_10: Optional[str]
+    isbn_13: Optional[str]
+    subtitle: Optional[str]
+
+
+def main() -> None:
+    """Main function"""
+
+    with open("data.json", "r") as file:
+        data = json.load(file)
+        print(data)
 
 
 if __name__ == '__main__':
-    say_hello()
+    main()
